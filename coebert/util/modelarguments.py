9d7c484d55e76f6753e7ace65d20f66aba1cd0ec
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-13 17:19:31 -0300

    Create modelarguments.py

---
+++ coebert/util/modelarguments.py
@@ -0,0 +1,64 @@
+# Import das bibliotecas.
+from dataclasses import dataclass, field
+from typing import Dict, Optional
+from typing import List
+
+@dataclass
+class ModelArguments:
+        
+    '''
+    Classe(ModelArguments) de definição dos parâmetros do modelo
+    '''
+ 
+    max_seq_len: Optional[int] = field(
+        default=None,
+        metadata={'help': 'max seq len'},
+    )    
+    pretrained_model_name_or_path: str = field(
+        default='neuralmind/bert-base-portuguese-cased',
+        metadata={'help': 'nome do modelo pré-treinado do BERT.'},
+    )
+    modelo_spacy: str = field(
+        default='pt_core_news_lg',
+        metadata={'help': 'nome do modelo do spaCy.'},
+    )
+    do_lower_case: bool = field(
+        default=False,
+        metadata={'help': 'define se o texto do modelo deve ser todo em minúsculo.'},
+    )  
+    output_attentions: bool = field(
+        default=False,
+        metadata={'help': 'habilita se o modelo retorna os pesos de atenção.'},
+    )
+    output_hidden_states: bool = field(
+        default=False,
+        metadata={'help': 'habilita gerar as camadas ocultas do modelo.'},
+    )
+    use_wandb : bool = field(
+        default=True,
+        metadata={'help': 'habilita o uso do wandb.'},
+    )
+    salvar_modelo_wandb : bool = field(
+        default=True,
+        metadata={'help': 'habilita o salvamento do modelo no wandb.'},
+    )
+    salvar_avaliacao : bool = field(
+        default=True,
+        metadata={'help': 'habilita o salvamento do resultado da avaliação.'},
+    )     
+    salvar_medicao : bool = field(
+        default=False,
+        metadata={'help': 'habilita o salvamento da medicao.'},
+    )
+    usar_mcl_ajustado : bool = field(
+        default=False,
+        metadata={'help': 'habilita o carragamento de mcl ajustado.'},
+    )
+    estrategia_medida: int = field(
+        default=0, # 0 - MEAN estratégia média / 1 - MAX  estratégia maior
+        metadata={'help': 'Estratégia de cálculo da médida dos embeddings.'},
+    )
+    filtro_palavra: int = field(
+        default=0, # 0 - Considera todas as palavras das sentenças / 1 - Desconsidera as stopwords / 2 - Considera somente as palavras substantivas
+        metadata={'help': 'Define o filtro de palavras das sentenças para gerar os embeddings.'},
+    )
