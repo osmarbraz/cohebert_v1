ffaebd099ed15dbe7afa467a52a59a9dd3406160
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-14 21:46:27 -0300

    Update medidas.py

--- coebert/medidor/medidas.py
+++ coebert/medidor/medidas.py
@@ -7,6 +7,7 @@ def similaridadeCoseno(documento1, documento2):
     '''
     Similaridade do cosseno dos embeddgins das sentenças.
     '''
+    
     similaridade = 1 - cosine(documento1, documento2)
     return similaridade
 
@@ -15,16 +16,16 @@ def distanciaEuclidiana(sentenca1, sentenca2):
     Distância euclidiana entre os embeddings das sentenças.
     Possui outros nomes como distância L2 ou norma L2.
     '''
-
+    
     distancia = euclidean(sentenca1, sentenca2)
     return distancia
-
 
 def distanciaManhattan(sentenca1, sentenca2):
     '''
     Distância Manhattan entre os embeddings das sentenças. 
     Possui outros nomes como distância Cityblock, distância L1, norma L1 e métrica do táxi.
     '''
+    
     distancia = cityblock(sentenca1, sentenca2)
 
     return distancia
