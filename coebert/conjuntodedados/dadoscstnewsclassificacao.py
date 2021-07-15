7762a69db926c7d96f697cf6977d7faf2726c229
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-15 14:11:02 -0300

    Update dadoscstnewsclassificacao.py

--- coebert/conjuntodedados/dadoscstnewsclassificacao.py
+++ coebert/conjuntodedados/dadoscstnewsclassificacao.py
@@ -1,1 +1,52 @@
+# Import das bibliotecas.
+import pandas as pd # Biblioteca pandas
 
+# Import de bibliotecas próprias
+from util.utilmodulo import *
+from util.utiltempo import *
+from util.utilarquivo import *
+
+from conjuntodedados.dadoscstnewsmedida import *
+
+def organizaDados(dfdados):
+    '''    
+    Organiza osdados do CSTNews para classificação e retorna um dataframe.
+    '''
+  
+    # Organiza os dados
+    dados_organizados = []
+
+    # Coloca o par um embaixo do outro.
+    for index, linha in dfdados.iterrows():        
+        # 1 Para original
+        dados_organizados.append((linha['idOriginal'],linha['documentoOriginal'],1))    
+        # 0 para uma permutação 
+        dados_organizados.append((linha['idPermutado'],linha['documentoPermutado'],0))
+
+    # Cria um dataframe com os dados
+    dfdados = pd.DataFrame(dados_organizados, columns=["id","documento","classe"])      
+
+    return dfdados 
+
+
+def getConjuntoDeDadosClassificacao(model_args, ORIGEM, tokenizer):  
+    '''    
+    Carrega os dados do CSTNews e retorna um dataframe para classificação.
+    '''
+    
+    # Realiza o download do conjunto de dados
+    downloadConjuntoDeDados(ORIGEM)
+    
+    # Carrega os pares de documentos dos arquivos
+    lista_documentos = carregaParesDocumentosCSTNews()
+        
+    # Converte em um dataframe
+    dfdados = converteListaParesDocumentos(lista_documentos)
+        
+    # Descarta os documentos muito grandes. (Que geram mais de 512 tokens)
+    dfdados = descartandoDocumentosMuitoGrandes(dfdados, model_args, tokenizer)
+    
+    # Organiza os dados para classificação
+    dfdados = organizaDados(dfdados)
+    
+    return dfdados
