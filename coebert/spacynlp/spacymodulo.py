f81a8d4bda47eaf7b6b7df2b1b04b15787ecaa2c
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-16 04:30:22 -0300

    Update spacymodulo.py

--- coebert/spacynlp/spacymodulo.py
+++ coebert/spacynlp/spacymodulo.py
@@ -10,85 +10,7 @@ from util.utilmodulo import *
 from util.utiltempo import *
 from util.utilarquivo import *
 
-def downloadSpacy(model_args):
-    '''
-    Realiza o download do arquivo do modelo para o diretório corrente.
-    Parâmetros:
-       `model_args` - Objeto com os argumentos do modelo.       
-    '''
-    
-    ARQUIVOMODELOSPACY = model_args.modelo_spacy
-    VERSAOSPACY = "-" + model_args.versao_spacy
-    NOME_ARQUIVO_MODELO_COMPACTADO = ARQUIVOMODELOSPACY + VERSAOSPACY + ".tar.gz"
-    
-    # Url do arquivo
-    URL_ARQUIVO_MODELO_COMPACTADO = "https://github.com/explosion/spacy-models/releases/download/" + ARQUIVOMODELOSPACY + VERSAOSPACY + "/" + NOME_ARQUIVO_MODELO_COMPACTADO
-
-    # Realiza o download do arquivo do modelo
-    downloadArquivo(URL_ARQUIVO_MODELO_COMPACTADO, NOME_ARQUIVO_MODELO_COMPACTADO)
-
-def descompactaSpacy(model_args):
-    '''
-    Descompacta o arquivo do modelo.
-    Parâmetros:
-       `model_args` - Objeto com os argumentos do modelo.       
-    '''
-    
-    ARQUIVOMODELOSPACY = model_args.modelo_spacy
-    VERSAOSPACY = "-" + model_args.versao_spacy
-    
-    # Nome do arquivo a ser descompactado
-    NOME_ARQUIVO_MODELO_COMPACTADO = ARQUIVOMODELOSPACY + VERSAOSPACY + ".tar.gz"
-    
-    arquivoTar = tarfile.open(NOME_ARQUIVO_MODELO_COMPACTADO, "r:gz")    
-    arquivoTar.extractall()    
-    arquivoTar.close()
-    
-    # Apaga o arquivo compactado
-    if os.path.isfile(NOME_ARQUIVO_MODELO_COMPACTADO):
-        os.remove(NOME_ARQUIVO_MODELO_COMPACTADO)
-    
-def moveSpacy(model_args):
-    '''
-    Coloca a pasta do modelo descompactado em uma pasta de nome mais simples.
-    Parâmetros:    
-       `model_args` - Objeto com os argumentos do modelo.           
-    '''
-    
-    ARQUIVOMODELOSPACY = model_args.modelo_spacy
-    VERSAOSPACY = "-" + model_args.versao_spacy
-    
-    # Caminho da origem do diretório
-    ORIGEM = "/content/" + ARQUIVOMODELOSPACY + VERSAOSPACY + "/" + ARQUIVOMODELOSPACY + "/" + ARQUIVOMODELOSPACY + VERSAOSPACY 
-    # Destino do diretório
-    DESTINO = "/content/" + ARQUIVOMODELOSPACY
-
-    shutil.move(ORIGEM, DESTINO)
-    
-def carregaSpacy(model_args):
-    '''
-    Realiza o carregamento do Spacy.  
-    Parâmetros:
-       `model_args` - Objeto com os argumentos do modelo.           
-    '''
-    
-    ARQUIVOMODELOSPACY = model_args.modelo_spacy
-    VERSAOSPACY = "-" + model_args.versao_spacy
-
-    downloadSpacy(model_args)
-
-    descompactaSpacy(model_args)
-    
-    moveSpacy(model_args)
-
-    # Caminho completo do modelo do spaCy
-    CAMINHOMODELOSPACY = '/content/' + ARQUIVOMODELOSPACY
-
-    # Necessário 'tagger' para encontrar os substantivos
-    nlp = spacy.load(CAMINHOMODELOSPACY, disable=['tokenizer', 'lemmatizer', 'ner', 'parser', 'textcat', 'custom'])
-
-    return nlp
-
+# ============================
 def getStopwords(nlp):
     '''
     Recupera as stop words do nlp(Spacy).
@@ -99,3 +21,97 @@ def getStopwords(nlp):
     spacy_stopwords = nlp.Defaults.stop_words
 
     return spacy_stopwords 
+
+# ============================
+def downloadSpacy(model_args):
+    '''
+    Realiza o download do arquivo do modelo para o diretório corrente.
+    Parâmetros:
+       `model_args` - Objeto com os argumentos do modelo.       
+    '''
+    
+    # Nome arquivo spacy
+    ARQUIVO_MODELO_SPACY = model_args.modelo_spacy
+    # Versão spaCy
+    VERSAO_SPACY = "-" + model_args.versao_spacy
+    # Nome arquivo compactado
+    NOME_ARQUIVO_MODELO_COMPACTADO = ARQUIVOMODELOSPACY + VERSAOSPACY + ".tar.gz"
+    
+    # Url do arquivo
+    URL_ARQUIVO_MODELO_COMPACTADO = "https://github.com/explosion/spacy-models/releases/download/" + ARQUIVO_MODELO_SPACY + VERSAO_SPACY + "/" + NOME_ARQUIVO_MODELO_COMPACTADO
+
+    # Realiza o download do arquivo do modelo
+    downloadArquivo(URL_ARQUIVO_MODELO_COMPACTADO, NOME_ARQUIVO_MODELO_COMPACTADO)
+
+# ============================   
+def descompactaSpacy(model_args):
+    '''
+    Descompacta o arquivo do modelo.
+    Parâmetros:
+       `model_args` - Objeto com os argumentos do modelo.       
+    '''
+    
+    # Nome arquivo spacy
+    ARQUIVO_MODELO_SPACY = model_args.modelo_spacy
+    # Versão spaCy
+    VERSAO_SPACY = "-" + model_args.versao_spacy
+    
+    # Nome do arquivo a ser descompactado
+    NOME_ARQUIVO_MODELO_COMPACTADO = ARQUIVO_MODELO_SPACY + VERSAO_SPACY + ".tar.gz"
+    
+    arquivoTar = tarfile.open(NOME_ARQUIVO_MODELO_COMPACTADO, "r:gz")    
+    arquivoTar.extractall()    
+    arquivoTar.close()
+    
+    # Apaga o arquivo compactado
+    if os.path.isfile(NOME_ARQUIVO_MODELO_COMPACTADO):
+        os.remove(NOME_ARQUIVO_MODELO_COMPACTADO)
+    
+# ============================
+def moveSpacy(model_args):
+    '''
+    Coloca a pasta do modelo descompactado em uma pasta de nome mais simples.
+    Parâmetros:    
+       `model_args` - Objeto com os argumentos do modelo.           
+    '''
+    
+    # Nome arquivo spacy
+    ARQUIVO_MODELO_SPACY = model_args.modelo_spacy
+    # Versão spaCy
+    VERSAO_SPACY = "-" + model_args.versao_spacy
+    
+    # Caminho da origem do diretório
+    ORIGEM = "/content/" + ARQUIVO_MODELO_SPACY + VERSAO_SPACY + "/" + ARQUIVO_MODELO_SPACY + "/" + ARQUIVO_MODELO_SPACY + VERSAO_SPACY 
+    # Destino do diretório
+    DESTINO = "/content/" + ARQUIVO_MODELO_SPACY
+
+    shutil.move(ORIGEM, DESTINO)
+
+# ============================    
+def carregaSpacy(model_args):
+    '''
+    Realiza o carregamento do Spacy.  
+    Parâmetros:
+       `model_args` - Objeto com os argumentos do modelo.           
+    '''
+    
+    # Nome arquivo spacy
+    ARQUIVO_MODELO_SPACY = model_args.modelo_spacy
+    # Versão spaCy
+    VERSAO_SPACY = "-" + model_args.versao_spacy
+    # Caminho completo do modelo do spaCy
+    DIRETORIO_MODELO_SPACY = '/content/' + ARQUIVO_MODELO_SPACY
+
+     # Verifica se o diretório existe
+    if os.path.exists(DIRETORIO_MODELO_SPACY) == false:
+        # Realiza o download do arquivo modelo do spaCy
+        downloadSpacy(model_args)
+        # Descompacta o spaCy
+        descompactaSpacy(model_args)
+        # Move o spaCy
+        moveSpacy(model_args)
+    
+    # Carrega o spaCy. Necessário somente 'tagger' para encontrar os substantivos
+    nlp = spacy.load(DIRETORIO_MODELO_SPACY, disable=['tokenizer', 'lemmatizer', 'ner', 'parser', 'textcat', 'custom'])
+
+    return nlp
