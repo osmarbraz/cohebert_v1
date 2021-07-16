d8d44f2362f268732550a84da0c2cc0f5c2ae136
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-16 04:32:35 -0300

    Update utilmodulo.py

--- coebert/util/utilmodulo.py
+++ coebert/util/utilmodulo.py
@@ -2,9 +2,12 @@
 import re # Biblioteca para expressão regular
 import unicodedata # Biblioteca para tratar codificação de caracteres
 
+# ============================  
 def removeAcentos(texto):   
     '''    
     Remove acentos de um texto.
+    Parâmetros:
+       `texto` - Texto a ser removido os acentos.
     '''
     try:
         text = unicode(texto, 'utf-8')
@@ -15,18 +18,24 @@ def removeAcentos(texto):
     texto = texto.decode("utf-8")
     return str(texto)
 
+# ============================  
 def limpaTexto(texto):    
     '''    
     Remove acentos e espaços e outros caracteres de um texto.
+    Parâmetros:
+       `texto` - Texto a ser limpo.
     '''
     texto = removeAcentos(texto.lower())
     texto = re.sub('[ ]+', '_', texto)
     texto = re.sub('[^.0-9a-zA-Z_-]', '', texto)
     return texto
     
+# ============================  
 def remove_tags(texto):
      '''
-     Remove tags de um texto
+     Remove tags de um texto.
+     Parâmetros:
+       `texto` - Texto com tags a serem removidas.      
      '''
      textoLimpo = re.compile('<.*?>')
      return re.sub(textoLimpo, '', texto)
