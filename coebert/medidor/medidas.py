e6ce56694264c3c2fb5b998538596e8b6dbacc9e
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-23 10:55:48 -0300

    Update medidas.py

--- coebert/medidor/medidas.py
+++ coebert/medidor/medidas.py
@@ -8,8 +8,8 @@ def similaridadeCoseno(texto1, texto2):
     '''
     Similaridade do cosseno dos embeddgins dos textos.
     Parâmetros:
-       `texto1` - Um texto a ser medido.           
-       `texto2` - Um texto a ser medido.                 
+        `texto1` - Um texto a ser medido.           
+        `texto2` - Um texto a ser medido.                 
     '''
     
     similaridade = 1 - cosine(texto1, texto2)
@@ -21,8 +21,8 @@ def distanciaEuclidiana(texto1, texto2):
     Distância euclidiana entre os embeddings dos textos.
     Possui outros nomes como distância L2 ou norma L2.
     Parâmetros:
-       `texto1` - Um texto a ser medido.           
-       `texto2` - Um texto a ser medido.
+        `texto1` - Um texto a ser medido.           
+        `texto2` - Um texto a ser medido.
     '''
     
     distancia = euclidean(texto1, texto2)
@@ -34,8 +34,8 @@ def distanciaManhattan(texto1, texto2):
     Distância Manhattan entre os embeddings das sentenças. 
     Possui outros nomes como distância Cityblock, distância L1, norma L1 e métrica do táxi.
     Parâmetros:
-       `texto1` - Um texto a ser medido.           
-       `texto2` - Um texto a ser medido.
+        `texto1` - Um texto a ser medido.           
+        `texto2` - Um texto a ser medido.
     '''
     
     distancia = cityblock(texto1, texto2)
