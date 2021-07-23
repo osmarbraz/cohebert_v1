824796710fd71636f9fa9867b8f7a17764b33f42
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-23 10:54:05 -0300

    Update experimentomodulo.py

--- coebert/experimento/experimentomodulo.py
+++ coebert/experimento/experimentomodulo.py
@@ -4,8 +4,18 @@ import pandas as pd
 
 # ============================
 def carregaMedidasCSTNews(DIRETORIO_MEDIDAS, TIPO_MODELO, ESTRATEGIA_POOLING, PALAVRA_RELEVANTE, NOME_MODELO_BERT, TAMANHO_BERT):
-  '''
-  Carrega as medidas de coerência de um diretório e retorna um dataframe.
+    '''
+    Carrega as medidas de coerência de um diretório e retorna um dataframe.
+    Parâmetros:
+        `DIRETORIO_MEDIDAS` - Diretório com os arquivos das medidas.    
+        `TIPO_MODELO` - Tipo do modelo(pretreinado ou ajustado) a ser carregado.  
+        `ESTRATEGIA_POOLING` - Nome da estratégia de pooling(MEAN ou MAX).
+        `PALAVRA_RELEVANTE` - Nome da estratégia de relevância(ALL, CLEAN ou NOUN).
+        `NOME_MODELO_BERT` - Nome do modelo(BERTimbau ou BERT) a ser carregado.  
+        `TAMANHO_BERT` - Tamanho do modelo(Base ou Large) a ser carregado. 
+        
+    Saída:
+        `dfMedida` - Um dataframe com os dados carregados.
   '''
   
   NOME_BASE = 'MedicaoCoerenciaCSTNews_v1'
