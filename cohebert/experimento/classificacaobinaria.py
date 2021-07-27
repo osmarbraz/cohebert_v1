0c3a86bfda2011d3bcaa05f077fe042c4cc90802
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-27 08:48:59 -0300

    Update classificacaobinaria.py

--- cohebert/experimento/classificacaobinaria.py
+++ cohebert/experimento/classificacaobinaria.py
@@ -16,7 +16,7 @@ from transformers import get_linear_schedule_with_warmup # Biblioteca do agendad
 from bert.bertmodulo  import *
 
 # ============================
-def carregaResultadoAvaliacao(model_args, training_args, DIRETORIO_AVALIACAO):
+def carregaResultadoAvaliacaoFold(model_args, training_args, DIRETORIO_AVALIACAO):
     '''
     Carrega e mostra os dados da avaliação. 
     
@@ -97,7 +97,7 @@ def carregaResultadoAvaliacao(model_args, training_args, DIRETORIO_AVALIACAO):
         logging.info('Diretório com os resultados não encontrado.')
 
 # ============================        
-def salvaResultadoClassificacao(model_args, DIRETORIO_CLASSIFICACAO, lista_resultado_avaliacao):
+def salvaResultadoClassificacaoFold(model_args, DIRETORIO_CLASSIFICACAO, lista_resultado_avaliacao):
     '''
     Salva o resultado da classificação. 
     
@@ -157,10 +157,69 @@ def salvaResultadoClassificacao(model_args, DIRETORIO_CLASSIFICACAO, lista_resul
             # Fecha o arquivo.
             arquivo.close()        
 
-# ============================        
-def salvaResultadoAvaliacao(model_args, training_args, DIRETORIO_AVALIACAO, acc, rec, pre, f1, vp_s, vn_s, fp_s, fn_s):
+# ============================               
+def salvaResultadoClassificacao(model_args, DIRETORIO_CLASSIFICACAO, lista_resultado_avaliacao):
     '''
     Salva os dados da avaliação. 
+    
+    Parâmetros:
+        `model_args` - Objeto com os argumentos do modelo. 
+        `training_args` - Objeto com os argumentos do treinamento. 
+        `DIRETORIO_AVALIACAO` - Diretório para salvar os dados da avaliação.        .        
+    '''          
+
+    if model_args.salvar_classificacao:
+
+        # Recupera a hora do sistema.
+        data_e_hora = datetime.datetime.now()
+
+        # Nome arquivo resultado
+        NOME_ARQUIVO_CLASSIFICACAO = training_args.output_dir + MODELO_BERT + TAMANHO_BERT
+  
+        # Verifica se o diretório existe
+        if not os.path.exists(DIRETORIO_CLASSIFICACAO):  
+            # Cria o diretório
+            os.makedirs(DIRETORIO_CLASSIFICACAO)
+            logging.info("Diretório criado: {}.".format(DIRETORIO_CLASSIFICACAO))
+        else:
+            logging.info("Diretório já existe: {}.".format(DIRETORIO_CLASSIFICACAO))
+
+        # Nome do arquivo a ser aberto.
+        NOME_ARQUIVO_CLASSIFICACAO_COMPLETO = DIRETORIO_CLASSIFICACAO + NOME_ARQUIVO_CLASSIFICACAO + ".csv"
+
+        # Gera todo o conteúdo a ser salvo no arquivo
+        novoConteudo = ""        
+        for resultado in lista_resultado_avaliacao:      
+            novoConteudo = novoConteudo + data_e_hora.strftime("%d/%m/%Y %H:%M") + ";" + str(resultado[0]) + ";" + str(resultado[1]) + ";" + str(resultado[2]) + "\n"
+
+        # Verifica se o arquivo existe.
+        if os.path.isfile(NOME_ARQUIVO_CLASSIFICACAO_COMPLETO):
+            logging.info("Atualizando arquivo classificação: {}.".format(NOME_ARQUIVO_CLASSIFICACAO_COMPLETO))
+            # Abre o arquivo para leitura.
+            arquivo = open(NOME_ARQUIVO_CLASSIFICACAO_COMPLETO,'r')
+            # Leitura de todas as linhas do arquivo.
+            conteudo = arquivo.readlines()
+            # Conteúdo a ser adicionado.
+            conteudo.append(novoConteudo)
+
+            # Abre novamente o arquivo (escrita).
+            arquivo = open(NOME_ARQUIVO_CLASSIFICACAO_COMPLETO,'w')
+            # escreva o conteúdo criado anteriormente nele.
+            arquivo.writelines(conteudo)  
+            # Fecha o arquivo.
+            arquivo.close()
+        else:
+            logging.info("Criando arquivo classificação: {}.".format(NOME_ARQUIVO_CLASSIFICACAO_COMPLETO))
+            # Abre novamente o arquivo (escrita).
+            arquivo = open(NOME_ARQUIVO_CLASSIFICACAO_COMPLETO,'w')
+            arquivo.writelines('data;id;classe;predicao\n' + novoConteudo)  # escreva o conteúdo criado anteriormente nele.
+            # Fecha o arquivo.
+            arquivo.close()            
+            
+# ============================        
+def salvaResultadoAvaliacaoFold(model_args, training_args, DIRETORIO_AVALIACAO, acc, rec, pre, f1, vp_s, vn_s, fp_s, fn_s):
+    '''
+    Salva os dados da avaliação de um fold. 
     
     Parâmetros:
         `model_args` - Objeto com os argumentos do modelo. 
@@ -215,7 +274,63 @@ def salvaResultadoAvaliacao(model_args, training_args, DIRETORIO_AVALIACAO, acc,
             arquivo.writelines('arquivo;data;tempo;acuracia;vp;vn;fp;fn\n' + novoConteudo)  # escreva o conteúdo criado anteriormente nele.
             # Fecha o arquivo.
             arquivo.close()
-        
+
+def salvaResultadoAvaliacao(model_args, training_args, DIRETORIO_AVALIACAO, acc, rec, pre, f1, vp_s, vn_s, fp_s, fn_s):
+     '''
+    Salva os dados da avaliação. 
+    
+    Parâmetros:
+        `model_args` - Objeto com os argumentos do modelo. 
+        `training_args` - Objeto com os argumentos do treinamento. 
+        `DIRETORIO_AVALIACAO` - Diretório para salvar os dados da avaliação.        .        
+    '''
+
+    if model_args.salvar_avaliacao:
+
+        # Recupera a hora do sistema.
+        data_e_hora = datetime.datetime.now()
+
+        # Nome arquivo resultado
+        NOME_ARQUIVO_AVALIACAO = training_args.output_dir + MODELO_BERT + TAMANHO_BERT
+
+        # Verifica se o diretório existe
+        if not os.path.exists(DIRETORIO_AVALIACAO):  
+            # Cria o diretório
+            os.makedirs(DIRETORIO_AVALIACAO)
+            logging.info("Diretório criado: {}.".format(DIRETORIO_AVALIACAO))
+        else:
+            logging.info("Diretório já existe: {}.".format(DIRETORIO_AVALIACAO))
+
+        # Nome do arquivo a ser aberto.
+        NOME_ARQUIVO_AVALIACAO_COMPLETO = DIRETORIO_AVALIACAO + NOME_ARQUIVO_AVALIACAO + ".csv"
+
+        # Conteúdo a ser adicionado.
+        novoConteudo = NOME_ARQUIVO_AVALIACAO + ";" + data_e_hora.strftime("%d/%m/%Y %H:%M") + ";"  + treinamento_total + ";"  + str(acc) + ";"  +  str(vp_s) + ";"  +  str(vn_s) + ";" +  str(fp_s) + ";" +  str(fn_s) + "\n"
+
+        # Verifica se o arquivo existe.
+        if os.path.isfile(NOME_ARQUIVO_AVALIACAO_COMPLETO):
+            logging.info("Atualizando arquivo resultado avaliação: {}.".format(NOME_ARQUIVO_AVALIACAO_COMPLETO))
+            # Abre o arquivo para leitura.
+            arquivo = open(NOME_ARQUIVO_AVALIACAO_COMPLETO,'r')
+            # Leitura de todas as linhas do arquivo.
+            conteudo = arquivo.readlines()
+            # Conteúdo a ser adicionado.
+            conteudo.append(novoConteudo)
+
+            # Abre novamente o arquivo (escrita).
+            arquivo = open(NOME_ARQUIVO_AVALIACAO_COMPLETO,'w')
+            # escreva o conteúdo criado anteriormente nele.
+            arquivo.writelines(conteudo)  
+            # Fecha o arquivo.
+            arquivo.close()
+        else:
+            logging.info("Criando arquivo resultado avaliação: {}.".format(NOME_ARQUIVO_AVALIACAO_COMPLETO))
+            # Abre novamente o arquivo (escrita).
+            arquivo = open(NOME_ARQUIVO_AVALIACAO_COMPLETO,'w')
+            arquivo.writelines('arquivo;data;tempo;acuracia;vp;vn;fp;fn\n' + novoConteudo)  # escreva o conteúdo criado anteriormente nele.
+            # Fecha o arquivo.
+            arquivo.close()            
+            
 # ============================
 def carregaOtimizador(training_args, model):
     '''
