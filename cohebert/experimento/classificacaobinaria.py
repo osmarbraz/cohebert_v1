0c6f05f497cbbabded2fa7017fce5fbadee89663
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-27 09:13:47 -0300

    Update classificacaobinaria.py

--- cohebert/experimento/classificacaobinaria.py
+++ cohebert/experimento/classificacaobinaria.py
@@ -18,7 +18,7 @@ from bert.bertmodulo  import *
 # ============================
 def carregaResultadoAvaliacaoFold(model_args, training_args, DIRETORIO_AVALIACAO):
     '''
-    Carrega e mostra os dados da avaliação. 
+    Carrega e mostra os dados da avaliação de folds. 
     
     Parâmetros:
         `model_args` - Objeto com os argumentos do modelo. 
@@ -97,9 +97,88 @@ def carregaResultadoAvaliacaoFold(model_args, training_args, DIRETORIO_AVALIACAO
         logging.info('Diretório com os resultados não encontrado.')
 
 # ============================        
+def carregaResultadoAvaliacao(model_args, training_args, DIRETORIO_AVALIACAO):
+    '''
+    Carrega e mostra os dados da avaliação. 
+    
+    Parâmetros:
+        `model_args` - Objeto com os argumentos do modelo. 
+        `training_args` - Objeto com os argumentos do treinamento. 
+        `DIRETORIO_AVALIACAO` - Diretório com os dados da avaliação.        .        
+    '''
+  
+    # Acumuladores.
+    somaAcuracia = 0
+    listaTempo = []
+    contaExecucoes = 0
+    
+     # Verifica o nome do modelo BERT a ser utilizado
+    MODELO_BERT = getNomeModeloBERT(model_args)
+
+    # Verifica o tamanho do modelo(default large)
+    TAMANHO_BERT =  getTamanhoBERT(model_args)
+
+    # Nome arquivo resultado
+    NOME_ARQUIVO_AVALIACAO = training_args.output_dir + MODELO_BERT + TAMANHO_BERT
+    
+    # Verifica se o diretório dos resultados existem.
+    if os.path.exists(DIRETORIO_AVALIACAO):
+        # Nome do arquivo mais o caminho
+        NOME_ARQUIVO_AVALIACAO_COMPLETO = DIRETORIO_AVALIACAO + NOME_ARQUIVO_AVALIACAO + ".csv"
+        # Verifica se o arquivo existe.
+        if os.path.isfile(NOME_ARQUIVO_AVALIACAO_COMPLETO):
+            # Carrega os dados do arquivo  
+            dados = pd.read_csv(NOME_ARQUIVO_AVALIACAO_COMPLETO, sep=';')
+
+            # Mostra os dados do teste da execução.
+            for index, linha in dados.iterrows():
+        
+                # Cálculo das estatísticas
+                acc = (linha['vp']+linha['vn'])/(linha['vp']+linha['vn']+linha['fp']+linha['fn'])
+                if (linha['vp']+linha['fn']) != 0:
+                    rec = (linha['vp'])/(linha['vp']+linha['fn'])
+                else:
+                    rec = 0
+                
+                if (linha['vp']+linha['fp']) != 0:
+                    pre = (linha['vp'])/(linha['vp']+linha['fp'])
+                else:  
+                    pre = 0
+                if (pre + rec) != 0:  
+                    f1 = 2 * ((pre * rec)/(pre + rec))
+                else:
+                    f1 = 0
+                qtdeTestes = linha['vp']+linha['vn']+linha['fp']+linha['fn']
+                logging.info("Arquivo: {}, Data: {}, Tempo:{}, QtdeTeste: {:3d}, Acc: {:.8f}, Rec: {:.8f}, Pre: {:.8f}, F1:{:.8f}, vp: {:4d}; vn: {:4d}; fp: {:4d}; fn: {:4d}".format(
+                        linha['arquivo'], linha['data'], linha['tempo'], qtdeTestes, acc, rec, pre, f1, linha['vp'], linha['vn'], linha['fp'], linha['fn']))  
+           
+                # Guarda o tempo.
+                listaTempo.append(str(linha['tempo']))
+
+                # Procura a maior acurácia.
+                somaAcuracia = somaAcuracia + acc
+
+                # Conta o número de execuções.
+                contaExecucoes = contaExecucoes + 1
+
+            # Mostra a soma da acurácia . 
+            logging.info("Total acurácia                                          : {:.8f}.".format(somaAcuracia))
+            # Mostra a quantidade de exeucões.
+            logging.info("Quantidade de execuções                                 : {}.".format(contaExecucoes))  
+            # Calcula a média.
+            media = somaAcuracia/contaExecucoes
+            logging.info("A média da acurácia de {:2d} execuções é                   : {:.8f}.".format(contaExecucoes, media))
+            logging.info("O tempo gasto na execução do treinamento {:2d} execuções é : {}.".format(contaExecucoes, somaTempo(listaTempo)))
+            logging.info("A média de tempo de {:2d} execuções é                      : {}.".format(contaExecucoes, mediaTempo(listaTempo)))
+        else:
+            logging.info("Arquivo com os resultados não encontrado")    
+    else:
+        logging.info("Diretório com os resultados não encontrado")          
+        
+# ============================        
 def salvaResultadoClassificacaoFold(model_args, DIRETORIO_CLASSIFICACAO, lista_resultado_avaliacao):
     '''
-    Salva o resultado da classificação. 
+    Salva o resultado da classificação de um fold. 
     
     Parâmetros:
         `model_args` - Objeto com os argumentos do modelo.         
