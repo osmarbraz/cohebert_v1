30ce28b0100b1e2f3becde98abd5f5df2a6c1780
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-28 18:13:46 -0300

    Create coherence_bert.py

---
+++ cohebert/coherence_bert.py
@@ -0,0 +1,124 @@
+# Biblioteca cohebert
+from bert.bertmodulo import *
+from bert.bertarguments import ModeloArgumentosMedida
+from experimento.calculomedida import *
+from spacynlp.spacymodulo import *
+
+# Definição dos parâmetros do Modelo para o cálculo de Medida
+model_args = ModeloArgumentosMedida(     
+    max_seq_len = 512,
+    #pretrained_model_name_or_path = 'neuralmind/bert-large-portuguese-cased',
+    pretrained_model_name_or_path = 'neuralmind/bert-base-portuguese-cased',
+    #pretrained_model_name_or_path = 'bert-base-multilingual-cased',
+    modelo_spacy = 'pt_core_news_lg',
+    versao_spacy = '2.3.0',
+    do_lower_case = False,   # default True
+    output_attentions = False,    # default False
+    output_hidden_states = True, # default False    
+    estrategia_pooling = 0, # 0 - MEAN estratégia média / 1 - MAX  estratégia maior
+    palavra_relevante = 0 # 0 - Considera todas as palavras das sentenças / 1 - Desconsidera as stopwords / 2 - Considera somente as palavras substantivas
+)
+
+# Constantes 
+ESTRATEGIA_POOLING = ['MEAN','MAX']
+PALAVRA_RELEVANTE = ['ALL', 'CLEAN', 'NOUN']
+
+
+class CoherenceBERT:
+    
+    # Construtor da classe
+    def __init__(self, pretrained_model_name_or_path):
+        # parâmetro recebido
+        model_args.pretrained_model_name_or_path = pretrained_model_name_or_path
+        
+        # Carrega o modelo e tokenizador do BERT        
+        self.model, self.tokenizer = carregaBERT(model_args)
+
+        if model_args.palavra_relevante != 0:
+            # Carrega o modelo spacy
+            self.nlp = carregaSpacy(model_args)
+        else:
+            self.nlp = None
+    
+    def defineEstrategiaPooling(estrategiaPooling):
+        if estrategiaPooling == ESTRATEGIA_POOLING[1]:
+            model_args.estrategia_pooling = 1
+        else:
+            model_args.estrategia_pooling = 0
+
+    def definePalavraRelevante(palavraRelevante):
+        if palavraRelevante == PALAVRA_RELEVANTE[1]:
+            model_args.palavra_relevante = 1
+        else:
+            if palavraRelevante == PALAVRA_RELEVANTE[2]:
+                model_args.palavra_relevante = 2
+            else:
+                model_args.palavra_relevante = 0
+
+
+    def getMedidaCoerencia(self, texto, estrategiaPooling = 'MEAN', palavraRelevante='ALL'):
+        ''' 
+        Retorna as medidas de (in)coerência Ccos, Ceuc, Cman do texto.
+        ''' 
+
+        defineEstrategiaPooling(estrategiaPooling)
+        definePalavraRelevante(palavraRelevante)
+
+        self.Ccos, self.Ceuc, self.Cman = getMedidasCoerenciaDocumento(texto, 
+                                                    modelo=self.model, 
+                                                    tokenizador=self.tokenizer, 
+                                                    nlp=self.nlp, 
+                                                    camada=listaTipoCamadas[4], 
+                                                    tipoDocumento='o', 
+                                                    estrategia_pooling=model_args.estrategia_pooling, 
+                                                    palavra_relevante=model_args.palavra_relevante)
+          
+        return self.Ccos, self.Ceuc, self.Cman
+
+    
+    def getMedidaCoerenciaCosseno(self, texto, palavraRelevante='ALL'):
+        ''' 
+        Retorna a medida de coerência do texto utilizando a medida de similaridade de cosseno.
+        ''' 
+
+        self.Ccos, self.Ceuc, self.Cman = getMedidasCoerenciaDocumento(texto, 
+                                                    modelo=self.model, 
+                                                    tokenizador=self.tokenizer, 
+                                                    nlp=self.nlp, 
+                                                    camada=listaTipoCamadas[4], 
+                                                    tipoDocumento='o', 
+                                                    estrategia_pooling=model_args.estrategia_pooling, 
+                                                    palavra_relevante=model_args.palavra_relevante)
+          
+        return self.Ccos
+    
+    def getMedidaCoerenciaEuclediana(self, texto, palavraRelevante='ALL'):
+        ''' 
+        Retorna a medida de incoerência do texto utilizando a medida de distância de Euclidiana.
+        ''' 
+
+        self.Ccos, self.Ceuc, self.Cman = getMedidasCoerenciaDocumento(texto, 
+                                                    modelo=self.model, 
+                                                    tokenizador=self.tokenizer, 
+                                                    nlp=self.nlp, 
+                                                    camada=listaTipoCamadas[4], 
+                                                    tipoDocumento='o', 
+                                                    estrategia_pooling=model_args.estrategia_pooling, 
+                                                    palavra_relevante=model_args.palavra_relevante)
+          
+        return self.Ceuc        
+    
+    def getMedidaCoerenciaManhattan(self, texto, palavraRelevante='ALL'):
+        ''' 
+        Retorna a medida de incoerência do texto utilizando a medida de distância de Manhattan.
+        ''' 
+        self.Ccos, self.Ceuc, self.Cman = getMedidasCoerenciaDocumento(texto, 
+                                                    modelo=self.model, 
+                                                    tokenizador=self.tokenizer, 
+                                                    nlp=self.nlp, 
+                                                    camada=listaTipoCamadas[4], 
+                                                    tipoDocumento='o', 
+                                                    estrategia_pooling=model_args.estrategia_pooling, 
+                                                    palavra_relevante=model_args.palavra_relevante)
+          
+        return self.Cman                
