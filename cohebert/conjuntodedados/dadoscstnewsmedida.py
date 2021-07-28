8103c34f13eaf7c7e4df96622d866e04a9d2f7e1
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2021-07-28 10:52:19 -0300

    Update dadoscstnewsmedida.py

--- cohebert/conjuntodedados/dadoscstnewsmedida.py
+++ cohebert/conjuntodedados/dadoscstnewsmedida.py
@@ -291,7 +291,7 @@ def converteListaParesDocumentos(lista_documentos):
     return dfdados
 
 # ============================   
-def descartandoDocumentosGrandes(model_args, tokenizer, dfdados):
+def descartandoDocumentosGrandes(dfdados, model_args, tokenizer):
     '''    
     Remove os documentos que extrapolam 512 tokens.
     Você pode definir o tamanho de documento que quiser no BERT, mas o modelo pré-treinado vem com um tamanho pré-definido. 
@@ -299,34 +299,44 @@ def descartandoDocumentosGrandes(model_args, tokenizer, dfdados):
     O tokenizador gera quantidades diferentes tokens para cada modelo pré-treinado. 
     Portanto é necessário especificar o tokenizador para descatar os documentos que ultrapassam o limite de tokens de entrada do BERT.
     Parâmetros:      
+        `dfdados` - Dataframe com os documentos a serem analisados.   
         `model_args` - Objeto com os argumentos do modelo.    
         `tokenizer` - Tokenizador BERT.
-        `dfdados` - Dataframe com os documentos a serem analisados.           
-        
     Saída:
         `dfdadosretorno` - Um dataframe sem os documentos grandes.
     '''
-  
-    # Tokenize a codifica os documentos para o BERT.     
-    dfdados['input_ids'] = dfdados['documentoOriginal'].apply(lambda tokens: tokenizer.encode(tokens, add_special_tokens=True))
-
-    # Reduz para o tamanho máximo suportado pelo BERT.
-    dfdados_512 = dfdados[dfdados['input_ids'].apply(len)<=model_args.max_seq_len]
-
-    # Remove as colunas desnecessárias.
-    dfdadosAnterior = dfdados.drop(columns=['input_ids'])
-    dfdadosretorno = dfdados_512.drop(columns=['input_ids'])
-
-    logging.info("Quantidade de dados anterior: {}.".format(len(dfdadosAnterior)))
-    logging.info("Nova quantidade de dados    : {}.".format(len(dfdadosretorno)))
-
-    # Mostra a quantidade registros removidos
-    dfdadosSemLista =  dfdadosretorno.drop(columns=['sentencasOriginais','sentencasPermutadas'])
-    dfdados512SemLista =  dfdadosAnterior.drop(columns=['sentencasOriginais','sentencasPermutadas'])
-
-    df = dfdados512SemLista.merge(dfdadosSemLista, how = 'outer' ,indicator=True).loc[lambda x : x['_merge']=='left_only']
     
-    logging.info("Quantidade de registros removidos: {}.".format(len(df)))
+    dfdadosretorno = None
+    
+    # Verifica se o tokenizador foi carregado
+    if tokenizer != None:
+    
+        # Define o tamanho máximo para os tokens
+        tamanho_maximo = model_args.max_seq_len
+  
+        # Tokenize a codifica os documentos para o BERT.     
+        dfdados['input_ids'] = dfdados['documentoOriginal'].apply(lambda tokens: tokenizer.encode(tokens, add_special_tokens=True))
+
+        # Reduz para o tamanho máximo suportado pelo BERT.
+        dfdados_512 = dfdados[dfdados['input_ids'].apply(len)<=tamanho_maximo]
+
+        # Remove as colunas desnecessárias.
+        dfdadosAnterior = dfdados.drop(columns=['input_ids'])
+        dfdadosretorno = dfdados_512.drop(columns=['input_ids'])
+
+        logging.info("Quantidade de dados anterior: {}.".format(len(dfdadosAnterior)))
+        logging.info("Nova quantidade de dados    : {}.".format(len(dfdadosretorno)))
+
+        # Remove colunas desnecessárias
+        dfdadosSemLista =  dfdadosretorno.drop(columns=['sentencasOriginais','sentencasPermutadas'])
+        dfdados512SemLista =  dfdadosAnterior.drop(columns=['sentencasOriginais','sentencasPermutadas'])
+
+        # Registros removidos
+        df = dfdados512SemLista.merge(dfdadosSemLista, how = 'outer' ,indicator=True).loc[lambda x : x['_merge']=='left_only']
+        logging.info("Quantidade de registros removidos: {}.".format(len(df)))
+        
+    else:
+        logging.info("Tokenizador não definido.")        
 
     return dfdadosretorno  
 
