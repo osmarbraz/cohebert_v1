4d708d71c2d0d9427a38e6870d60c0fadb27c312
osmar <osmarbraz@hotmail.com>
2021-08-01 09:28:45 -0300

    Formatação da documentação.

--- cohebert/util/utilarquivo.py
+++ cohebert/util/utilarquivo.py
@@ -1,6 +1,7 @@
 # Import das bibliotecas.
 import logging  # Biblioteca de logging
 import requests # Biblioteca de download
+from tqdm.notebook import tqdm as tqdm_notebook # Biblioteca para barra de progresso
 
 # Import de bibliotecas próprias
 from util.utilmodulo import *
@@ -17,7 +18,41 @@ def downloadArquivo(url_arquivo, nome_arquivo_destino):
     '''
     
     # Realiza o download de um arquivo em uma url
+    data = requests.get(url_arquivo, stream=True)
+    
+    if req.status_code != 200:
+        loggin.info("Exceção ao tentar realizar download {}. Response {}".format(url, req.status_code), file=sys.stderr)
+        req.raise_for_status()
+        return
+
+    caminho_download = nome_arquivo_destino + "_part"
+    
+    with open(caminho_download, "wb") as file_binary:
+        
+        content_length = req.headers.get('Content-Length')        
+        total = int(content_length) if content_length is not None else None        
+        progress = tqdm_notebook(unit="B", total=total, unit_scale=True)        
+        for chunk in req.iter_content(chunk_size=1024):        
+            if chunk: 
+                progress.update(len(chunk))
+                file_binary.write(chunk)
+
+    os.rename(caminho_download, nome_arquivo_destino)
+    progress.close()
+
+# ============================  
+def downloadArquivo1(url_arquivo, nome_arquivo_destino):
+    '''    
+    Realiza o download de um arquivo de uma url em salva em nome_arquivo_destino.
+    
+    Parâmetros:
+    `url_arquivo` - URL do arquivos a ser feito download.      
+    `nome_arquivo_destino` - Nome do arquivo a ser salvo.      
+    '''
+    
+    # Realiza o download de um arquivo em uma url
     data = requests.get(url_arquivo)
+    
     # Salva em um arquivo
     arquivo = open(nome_arquivo_destino, 'wb')
     arquivo.write(data.content)
