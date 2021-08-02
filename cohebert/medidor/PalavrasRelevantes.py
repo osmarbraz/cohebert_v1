24dac702b430cc17d91797dca572ff0d051f0c17
osmar <osmarbraz@hotmail.com>
2021-08-02 07:13:53 -0300

    Criação enum constantes.

---
+++ cohebert/medidor/PalavrasRelevantes.py
@@ -0,0 +1,9 @@
+# Import das bibliotecas.
+import logging  # Biblioteca de logging
+from enum import Enum # Biblioteca de Enum
+
+class PalavrasRelevantes(Enum):
+    ALL = 0 # Todas as palavras
+    CLEAN = 1 # Sem stopwords
+    NOUN = 2 # Somente substantivos
+    
